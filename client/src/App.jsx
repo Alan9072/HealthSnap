9295a3e9fbee7f2b416d7fcc9e55209e83e17956
Alan Joy <alanjoy456@gmail.com>
2025-01-11 16:34:55 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home.jsx";
+// import QRScanner from "./pages/QRScanner";
+// import ProductDetails from "./pages/ProductDetails";
+// import Profile from "./pages/Profile";
+// import Settings from "./pages/Settings";
+import Navbar from "./components/Navbar/Navbar";
+import styles from './App.module.css'
+
+const App = () => {
+  return (
+    <Router>
+      <div className={styles.appwrapper}>
+        
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/scan" element={<QRScanner />} />
+            <Route path="/product" element={<ProductDetails />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/settings" element={<Settings />} /> */}
+          </Routes>
+        
+        <Navbar />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "./Navbar.module.css";
+import { BsUpcScan } from "react-icons/bs";
+import { IoSettingsSharp } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { IoHome } from "react-icons/io5";
+import { MdHistory } from "react-icons/md";
+
+function Navbar() {
+  return (
+    <div className={style.mainDiv}>
+      <Link to="/" className={style.navItem}>
+       <IoHome className={style.reactIcons}/>
+      </Link>
+      <Link to="/scan" className={style.navItem}>
+        <MdHistory className={style.reactIcons}/>
+      </Link>
+      <Link to="/product" className={style.navItem}>
+        <BsUpcScan className={style.reactIcons} />
+      </Link>
+      <Link to="/profile" className={style.navItem}>
+        <CgProfile className={style.reactIcons}/>
+      </Link>
+      <Link to="/settings" className={style.navItem}>
+      <IoSettingsSharp className={style.reactIcons}/>
+      </Link>
+    </div>
+  );
+}
+
+export default Navbar;
