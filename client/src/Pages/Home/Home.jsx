c39f58ba98d7c0d5ecf190208f6f6560e667482b
Alan Joy <alanjoy456@gmail.com>
2025-01-11 19:18:25 +0530

    made the search form in home page

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,30 +1,24 @@
 import React from 'react'
 import styles from './Home.module.css';
+import { IoSearchOutline } from "react-icons/io5";
 
 function Home() {
   return (
     <div className={styles.homeDiv}>{/* Every main page should have a main div that accumulates all inner content, the styles of these also should be same .*/}
+      <div className={styles.productTitle}>
+        <p>Products</p>
+        <form className={styles.searchBar}>
+          <input type="text" placeholder='Search your products'/>
+          <IoSearchOutline style={{
+          position: "absolute",
+          top: "50%",
+          right: "15px",
+          transform: "translate(0%, -50%)"
+        }}/>
+        </form>
+      </div>
       <div className={styles.homeContent}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora pariatur quaerat sunt reprehenderit ab quas quam quasi vero consequuntur ducimus aliquam maxime porro iusto ipsam temporibus maiores dignissimos, rem deserunt?
-        Obcaecati dolores, minima porro ut soluta impedit eum eveniet molestiae perspiciatis laborum, autem magni dicta excepturi similique debitis ipsam accusamus, voluptatem nam aliquid iusto. Non pariatur libero neque amet eaque.
-        Mollitia corrupti ipsam minus veritatis aspernatur sed, rem debitis magnam suscipit pariatur quasi libero labore id, voluptas reprehenderit similique nesciunt porro aut voluptate ullam nemo modi dicta illo dolor? Sit.
-        Inventore itaque rem quae, sequi odio harum autem, voluptatem exercitationem cumque unde obcaecati architecto eveniet ea tempore aut quia fugit suscipit. Quaerat sint nostrum blanditiis nemo necessitatibus officiis quis vero?
-        Placeat incidunt pariatur autem sint, recusandae laboriosam dicta beatae dolor fuga. Temporibus provident reiciendis veritatis voluptatum sint atque optio, voluptatibus at veniam voluptates! Porro quia illo hic fugiat, assumenda quam.
-        Laborum eos amet minus, sint iusto a consectetur incidunt hic adipisci quisquam quos ipsam dicta doloribus aperiam qui soluta aut impedit porro suscipit repellendus dolorum delectus! Fuga voluptatum libero distinctio.
-        Impedit sequi quidem nesciunt dignissimos sunt, voluptas quas. Corporis ex quam minus laudantium tempore aperiam numquam officiis aut sit, dolor, eveniet error! Nisi ullam quibusdam reprehenderit magnam pariatur fugit nam?
-        Magni, nemo modi sint veniam quidem exercitationem enim eveniet porro suscipit corporis veritatis in accusantium at, architecto nisi cumque. Ipsa ea magni officiis recusandae dolores rem aliquam rerum voluptatem quaerat?
-        Deleniti esse commodi ducimus ipsam molestias autem sit praesentium libero doloremque error rem, culpa laborum, placeat facere similique? Provident deserunt eius dignissimos fugit voluptates fugiat non ex similique voluptatem deleniti.
-        Nostrum aspernatur ipsum ad hic. Animi explicabo molestias doloremque, aut consectetur eaque, reprehenderit impedit consequatur ipsum iure voluptates porro assumenda adipisci! Beatae rem architecto nemo incidunt inventore dignissimos est dolor!
-        Alias quas magni assumenda, facere nulla iste at quae, saepe nesciunt quia eum, dolorem amet rerum esse architecto rem expedita temporibus eius vitae. Cupiditate totam, corporis voluptatum impedit maxime aperiam?
-        Fuga minima porro quos. Quam corporis, autem eligendi aliquam dolorum at, tempore soluta ipsam molestiae eos laborum quisquam voluptatibus, repellendus quas eum minus incidunt? Quibusdam fuga eveniet aperiam accusamus officia.
-        Nam delectus exercitationem culpa aut at voluptatibus necessitatibus perferendis rem voluptates iusto asperiores numquam quia perspiciatis eaque, quod sint laboriosam eius blanditiis recusandae! Aspernatur fuga illo amet, iste rerum perspiciatis!
-        Deleniti recusandae aspernatur cumque ullam placeat a autem architecto quod magnam nobis, beatae, maiores, laboriosam nulla sunt? Ea libero sed officiis! Eius nobis eaque quidem? Voluptatem provident eligendi ab nostrum?
-        Exercitationem veritatis nulla velit, tempore voluptatum dicta aut expedita quia? Ad ratione odio quis minima animi quia totam tempora voluptate est earum dolore, quaerat recusandae itaque optio nisi, distinctio eius.
-        Iure cupiditate atque necessitatibus, illo ea iusto delectus autem ex provident hic odio maiores doloremque eligendi rem consectetur laboriosam porro non quae dicta natus nam vel. Dicta eligendi aliquam voluptas?
-        Hic, magnam minima veritatis explicabo dolor voluptatum ducimus sunt ipsum aut tempora temporibus, assumenda modi aperiam vitae sequi repudiandae beatae doloribus doloremque a. Reiciendis doloribus perferendis rerum, expedita est inventore?
-        Et, commodi delectus beatae quibusdam expedita tempora exercitationem laborum labore laboriosam sapiente, recusandae quod magni sed fugit. Iure nostrum tempore quibusdam consequatur distinctio ipsum quasi natus reiciendis, quaerat, atque doloremque?
-        Velit id delectus deserunt at cumque iure perspiciatis dolor quaerat commodi deleniti, beatae harum, asperiores mollitia sequi veritatis rerum sed debitis sunt! Id laudantium quaerat accusantium dignissimos laborum libero nobis!
-        Libero magni, quisquam consequatur nemo recusandae nihil! Voluptate dolorem libero excepturi veritatis obcaecati voluptates distinctio sunt, debitis expedita in blanditiis nobis assumenda. Quod, pariatur animi? Placeat atque totam iusto accusantium?
+        
       </div>
     </div>
   );
