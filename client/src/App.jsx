9793eca400eb551d092c00defc9e27cbeafe4b49
Alan Joy <alanjoy456@gmail.com>
2025-02-18 22:53:36 +0530

    refactor page transition animations for simplified opacity effects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,10 +18,15 @@ import NotFound from "./Pages/NotFound/NotFound.jsx";
 
 
 const pageVariants = {
-  initial: { opacity: 0, y: 10 },
-  animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: -10 },
+  initial: { opacity: 0},
+  animate: { opacity: 1},
+  exit: { opacity: 0 },
 };
+// const pageVariants = {
+//   initial: { opacity: 0, y: 10 },
+//   animate: { opacity: 1, y: 0 },
+//   exit: { opacity: 0, y: -10 },
+// };
 
 const transitionSettings = {
   duration: 0.15,
