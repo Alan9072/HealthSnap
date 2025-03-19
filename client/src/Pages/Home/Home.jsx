d0df8a9aeb99cb359de2e94700563dababa53062
Alan Joy <alanjoy456@gmail.com>
2025-03-19 21:25:45 +0530

    feat: add main logo to Home component and enhance layout with new icons and styling

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,39 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import styles from './Home.module.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import styles from "./Home.module.css";
 import { IoSearchOutline } from "react-icons/io5";
-import Loading from '../../components/Loading/Loading';
+import Loading from "../../components/Loading/Loading";
+import { GoGitCompare } from "react-icons/go";
+import { IoScan } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { GoHistory } from "react-icons/go";
 
 function Home() {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate(); // Add navigation
 
-
   return (
     <div className={styles.homeDiv}>
       <div className={styles.productTitle}>
-        <p>Foods By Categories</p>
+        <div className={styles.logo}></div>
       </div>
       <div className={styles.homeContent}>
         <div className={styles.name}>
-          <p>Hii Alan</p>
+          <p>Hii, Alan</p>
           <p>Want to scan a new Product ?</p>
         </div>
         <div className={styles.tipBox}>
           <div className={styles.tagline}>
             <p>Health starts with a Scan!</p>
-            <p>Quickly check food quality, allergens, and health impact before you buy.</p>
-            <p>Scan Now</p> 
+            <p>
+              Quickly check food quality, allergens, and health impact before
+              you buy.
+            </p>
+            <p>Scan Now</p>
           </div>
           <div className={styles.img}></div>
         </div>
         <div className={styles.navBox}>
-          <div className={styles.eachBox}></div>
-          <div className={styles.eachBox}></div>
-          <div className={styles.eachBox}></div>
-          <div className={styles.eachBox}></div>
+          <div className={styles.eachBox}>
+            <div className={styles.iconBox}>
+              <div className={styles.icon}>
+                <IoScan size={17} color="white"/>
+              </div>
+            </div>
+            <div className={styles.content}>
+              <p>Scan</p>
+              <p> Instantly scan barcodes for product details.</p>
+            </div>
+          </div>
+          <div className={styles.eachBox}>
+            <div className={styles.iconBox}>
+              <div className={styles.icon}>
+                <CgProfile size={17} color="white"/>
+              </div>
+            </div>
+            <div className={styles.content}>
+              <p>Profile</p>
+              <p>View and manage your health preferences.</p>
+            </div>
+          </div>
+          <div className={styles.eachBox}>
+            <div className={styles.iconBox}>
+              <div className={styles.icon}>
+                <GoHistory size={17} color="white"/>
+              </div>
+            </div>
+            <div className={styles.content}>
+              <p>History</p>
+              <p>Track previously scanned food items.</p>
+            </div>
+          </div>
+          <div className={styles.eachBox}>
+            <div className={styles.iconBox}>
+              <div className={styles.icon}>
+                <GoGitCompare size={17} color="white"/>
+              </div>
+            </div>
+            <div className={styles.content}>
+              <p>Compare</p>
+              <p>Compare products for better choices.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
